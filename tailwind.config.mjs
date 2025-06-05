a26525c20714b4059241c130398dfcee26efb103
Erik Billebjer Ulrikson <erik.ulrikson@gmail.com>
2025-06-05 19:26:53 +0200

    Add missing Tailwind CSS config

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tw-animate-css")],
+};
+export default config;
